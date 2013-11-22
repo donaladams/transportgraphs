ce71d2f751f63f9312e8d8423d24dc94cfe4ea03
William Adams <adamsw@tcd.ie>
2013-11-22 12:17:22 +0000

    Added more converters and tests needed for building correct gtfs.model instances.

--- transportgraphs/tests/typeconverter_tests.py
+++ transportgraphs/tests/typeconverter_tests.py
@@ -1,14 +1,20 @@
+import pytz
+import datetime
 from nose.tools import raises
-from utils.typeconverters import UnicodeToFloatConverter
-from utils.typeconverters import UnicodeToIntConverter
+from utils.typeconverters import UnicodeToFloat
+from utils.typeconverters import UnicodeToInt
+from utils.typeconverters import UnicodeCheck
+from utils.typeconverters import UnicodeToTimeZone
+from utils.typeconverters import UnicodeToDatetime
+from utils.typeconverters import UnicodeToTime
 
-class TestUnicodeToFloatConverter(object):
-    """ Tests the functionality of UnicodeToFloatConverter"""
+class TestUnicodeToFloat(object):
+    """ Tests the functionality of UnicodeToFloat """
 
     def test_pass_on_valid_input_a(self):
         unicode_string = u"1.65"
 
-        converter = UnicodeToFloatConverter()
+        converter = UnicodeToFloat()
         output = converter.convert(unicode_string)
 
         assert type(output) is float
@@ -17,7 +23,7 @@ class TestUnicodeToFloatConverter(object):
     def test_pass_on_valid_input_b(self):
         unicode_string = u"1000"
 
-        converter = UnicodeToFloatConverter()
+        converter = UnicodeToFloat()
         output = converter.convert(unicode_string)
 
         assert type(output) is float
@@ -27,27 +33,28 @@ class TestUnicodeToFloatConverter(object):
     def test_fail_on_conversion_impossible(self):
         unicode_string = u"I'm a unicode string"
 
-        converter = UnicodeToFloatConverter()
+        converter = UnicodeToFloat()
         output = converter.convert(unicode_string)
-
+        #should not reach here
         assert False
 
     @raises(ValueError)
     def test_fail_on_input_not_unicode(self):
         input_value = None
 
-        converter = UnicodeToFloatConverter()
+        converter = UnicodeToFloat()
         output = converter.convert(input_value)
-
+        #should not reach here
         assert False
 
-class TestUnicodeToIntConverter(object):
-    """ Tests the functionality of UnicodeToFloatConverter"""
+
+class TestUnicodeToInt(object):
+    """ Tests the functionality of UnicodeToInt """
 
     def test_pass_on_valid_input_a(self):
         unicode_string = u"1"
 
-        converter = UnicodeToIntConverter()
+        converter = UnicodeToInt()
         output = converter.convert(unicode_string)
 
         assert type(output) is int
@@ -56,7 +63,7 @@ class TestUnicodeToIntConverter(object):
     def test_pass_on_valid_input_b(self):
         unicode_string = u"-21000"
 
-        converter = UnicodeToIntConverter()
+        converter = UnicodeToInt()
         output = converter.convert(unicode_string)
 
         assert type(output) is int
@@ -66,16 +73,114 @@ class TestUnicodeToIntConverter(object):
     def test_fail_on_conversion_impossible(self):
         unicode_string = u"I'm a unicode string"
 
-        converter = UnicodeToIntConverter()
+        converter = UnicodeToInt()
         output = converter.convert(unicode_string)
-
+        #should not reach here
         assert False
 
     @raises(ValueError)
     def test_fail_on_input_not_unicode(self):
         input_value = None
 
-        converter = UnicodeToIntConverter()
+        converter = UnicodeToInt()
         output = converter.convert(input_value)
-
+        #should not reach here
         assert False
+
+
+class TestUnicodeCheck(object):
+    """ Tests the UnicodeCheck converter class """
+
+    @raises(ValueError)
+    def test_non_unicode_input(self):
+        python_string = "I'm a python string"
+        converter = UnicodeCheck()
+        output = converter.convert(python_string)
+        #should not reach here
+        assert False
+
+    def test_unicode_input(self):
+        unicode_string = u"I'm a unicodestring"
+
+        converter = UnicodeCheck()
+        output = converter.convert(unicode_string)
+
+        assert type(output) is unicode
+        assert output == unicode_string
+
+
+class TestUnicodeToTimeZone(object):
+    """ Tests the UnicodeToTimeZone class """
+
+    def test_valid_input(self):
+        """ This should correctly produce a class
+            derived from datetime.tzinfo """
+        unicode_string = u"Europe/Dublin"
+
+        converter = UnicodeToTimeZone()
+        output = converter.convert(unicode_string)
+
+        assert issubclass(type(output), datetime.tzinfo)
+        assert output.zone == unicode_string
+
+    @raises(ValueError)
+    def test_invalid_input(self):
+        """ This should cause the converter to throw
+            a ValueError"""
+        unicode_string = u"I'm definitely not a time zone name"
+
+        converter = UnicodeToTimeZone()
+        output = converter.convert(unicode_string)
+
+
+class TestUnicodeToDatetime(object):
+    """ Test conversion of unicode to datetime """
+
+    def test_correct_gtfs_date_format_input(self):
+        unicode_string = u"20130520"
+        converter = UnicodeToDatetime("%Y%m%d")
+        output = converter.convert(unicode_string)
+
+        assert type(output) == datetime.datetime
+        assert output.year == 2013
+        assert output.month == 5
+        assert output.day == 20
+        assert output.hour == 0
+        assert output.minute == 0
+        assert output.second == 0
+
+    @raises(ValueError)
+    def test_bad_gtfs_format_input(self):
+        """ Should through a ValueError as date incorrect"""
+        unicode_string = u"adcd201212"
+        converter = UnicodeToDatetime("%Y%m%d")
+        output = converter.convert(unicode_string)
+        #should not reach here
+        assert False
+
+class TestUnicodeToTime(object):
+    """ Test conversion of unicode to datetime.time """
+
+    def test_correct_gtfs_time_format_input(self):
+        unicode_string = u"10:05:10"
+        converter = UnicodeToTime("%H:%M:%S")
+        output = converter.convert(unicode_string)
+
+        assert type(output) == datetime.time
+        assert output.hour == 10
+        assert output.minute == 5
+        assert output.second == 10
+
+
+    @raises(ValueError)
+    def test_bad_gtfs_format_input(self):
+        """ Should through a ValueError as time incorrect"""
+        unicode_string = u"1:65:0a"
+        converter = UnicodeToTime("%Y%m%d")
+        output = converter.convert("%H:%M:%S")
+        #should not reach here
+        assert False
+
+
+
+
--- transportgraphs/utils/typeconverters.py
+++ transportgraphs/utils/typeconverters.py
@@ -1,7 +1,14 @@
+"""
+typeconverters
+
+This module contains classes used for converting between
+different types
+"""
+
 from abc import ABCMeta, abstractmethod
-from exceptions import NotImplementedError
-from exceptions import ValueError
 from utils.decorators import check_value_is_unicode
+from datetime import datetime, timedelta
+import pytz
 
 class UnicodeToTypeConverter(object):
     """ Base class for performing type conversion """
@@ -11,21 +18,70 @@ class UnicodeToTypeConverter(object):
     def convert(self, value):
         """ Perform the conversion or raise an exception. This
             should be defined in the base class. """
-        raise NotImplementedError
+        raise NotImplementedError()
 
-class UnicodeToFloatConverter(UnicodeToTypeConverter):
+
+class UnicodeCheck(UnicodeToTypeConverter):
+    """ Just used to check the value is indeed unicode. """
+
+    @check_value_is_unicode
+    def convert(self, value):
+        return value
+
+
+class UnicodeToFloat(UnicodeToTypeConverter):
     """ Converts form unicode to float """
 
     @check_value_is_unicode
     def convert(self, value):
         return float(value)
 
-class UnicodeToIntConverter(UnicodeToTypeConverter):
+
+class UnicodeToInt(UnicodeToTypeConverter):
     """ Converts form unicode to int """
 
     @check_value_is_unicode
     def convert(self, value):
         return int(value)
 
+
+class UnicodeToDatetime(UnicodeToTypeConverter):
+    """ Converts a unicode string into datetime if
+        conversion is possible. """
+
+    def __init__(self, format='%Y%m%d'):
+        self.format = format
+
+    @check_value_is_unicode
+    def convert(self, value):
+        return datetime.strptime(value, self.format)
+
+class UnicodeToTime(UnicodeToTypeConverter):
+    """ Converts a unicode string into time if
+        conversion is possible. """
+
+    def __init__(self, format='%H:%M:%S'):
+        self.format = format
+
+    @check_value_is_unicode
+    def convert(self, value):
+        dt = datetime.strptime(value, self.format)
+        return dt.time()
+
+class UnicodeToTimeZone(UnicodeToTypeConverter):
+    """ Converts a name from the tz database
+        (http://en.wikipedia.org/wiki/List_of_tz_zones)
+        into a pytz timezone object
+        """
+    @check_value_is_unicode
+    def convert(self, value):
+        try:
+            tz = pytz.timezone(value)
+            return tz
+        except pytz.UnknownTimeZoneError:
+            # raise Value error instead to be consistent
+            raise ValueError(
+                u"{0} is not a valid timezone name.".format(value)
+                )
 
 
